436c1066f2abe47b0d73fc0f6f531421123925ab
KaelthasSS <60739883+KaelthasSS@users.noreply.github.com>
2020-02-25 04:17:31 +0000

    Add files via upload
    Code Upload

---
+++ Sean_OOP_ASS1/Sean_OOP_ASS1/ClassMethod.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sean_OOP_ASS1
+{
+    //https://www.w3schools.com/cs/cs_abstract.asp [ABSTRACT METHODS TO THIS FILE]
+    //A class for each country
+    public class Country
+    {
+        //DATA MEMBERS
+        //------------------------------------------------------
+        //Set encapsulation preventing country name reassignment (State Variable)
+        public string Name { get; private set; } = "";
+
+        //No encapsulation on last three variables as we will be manipulating both in other .cs files
+        public float Population = 0.0f;
+        public char Vote = 'x';
+        public bool Participation = true;
+
+        //Object Constructor
+        public Country(string Cname, float Cpopulation, char Cvote, bool Cparticipation)
+        {
+            //Link class variables
+            Name = Cname;
+            Population = Cpopulation;
+            Vote = Cvote;
+            Participation = Cparticipation;
+        }
+
+        public static void ParticipatingCountries()
+        {
+
+        }
+    }
+}
