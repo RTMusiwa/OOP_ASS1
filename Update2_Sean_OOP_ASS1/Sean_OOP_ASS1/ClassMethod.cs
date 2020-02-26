0fc46fd5b107c4a707d9545d8b6aa96968db2895
KaelthasSS <60739883+KaelthasSS@users.noreply.github.com>
2020-02-26 12:16:38 +0000

    Add files via upload
    Update2

---
+++ Update2_Sean_OOP_ASS1/Sean_OOP_ASS1/ClassMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sean_OOP_ASS1
+{
+    //A class for each country
+    class Country
+    {
+        //Data members
+        public string Name { get; private set; } = ""; //set envapsulation preventing country name and population assignment
+        public float Population { get; private set; } = 0.0f;
+        public int Vote = 0; //Public variable for reassigning out of this class
+
+        //Object Constructor
+        public Country(string name, float population)
+        {
+            Name = name;
+            Population = population;
+        }
+    }
+}
---
+++ Update2_Sean_OOP_ASS1/Sean_OOP_ASS1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Sean_OOP_ASS1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Object instantiation
+            Country c01 = new Country("Austria", 1.98f);
+            Country c02 = new Country("Belgium", 2.56f);
+            Country c03 = new Country("Bulgaria", 1.56f);
+            Country c04 = new Country("Croatia", 0.91f);
+            Country c05 = new Country("Cyprus", 0.2f);
+            Country c06 = new Country("Czech Republic", 2.35f);
+            Country c07 = new Country("Denmark", 1.3f);
+            Country c08 = new Country("Estonia", 0.3f);
+            Country c09 = new Country("Finland", 1.23f);
+            Country c10 = new Country("France", 14.98f);
+            Country c11 = new Country("Germany", 18.54f);s
+            Country c12 = new Country("Greece", 2.4f);
+            Country c13 = new Country("Hungary", 2.18f);
+            Country c14 = new Country("Ireland", 1.1f);
+            Country c15 = new Country("Italy", 13.65f);
+            Country c16 = new Country("Latvia", 0.43f);
+            Country c17 = new Country("Lithuania", 0.62f);
+            Country c18 = new Country("Luxembourg", 0.14f);
+            Country c19 = new Country("Malta", 0.11f);
+            Country c20 = new Country("Netherlands", 3.89f);
+            Country c21 = new Country("Poland", 8.49f);
+            Country c22 = new Country("Portugal", 2.3f);
+            Country c23 = new Country("Romania", 4.34f);
+            Country c24 = new Country("Slovakia", 1.22f);
+            Country c25 = new Country("Slovenia", 0.47f);
+            Country c26 = new Country("Spain", 10.49f);
+
+            //country iteration requuired for easy voting rule/countries participating
+        }
+    }
+}
